2d2d3660e58f62fe656e93c4b3f0e5867db24e74
hadi osama <hadiosama622@gmail.com>
2025-07-02 10:27:43 +0300

    added a timer and flip card move

--- js/app.js
+++ js/app.js
@@ -2,12 +2,36 @@
 
 
 /*---------- Variables (state) ---------*/
-
+let timeLeft = 120;
 
 /*----- Cached Element References  -----*/
-
+timeEl = document.querySelector('#timer')
 
 /*-------------- Functions -------------*/
+
+function init() {
+
+}
+
+function gameTime() {
+const intervalId = setInterval(() => {
+    if (timeLeft >= 0) {
+ document.getElementById("timer").innerHTML = `Timer: ${timeLeft--}s`;
+    }
+   
+
+}, 2000)
+
+}
+ gameTime()
+function reset() {
+
+}
+
+function checkCards() {
+
+}
+
 
 
 /*----------- Event Listeners ----------*/
